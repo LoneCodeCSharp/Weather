90f6df0b8f092d6e1fc6046b50530d6cda55b2d9
Zeni <Zeni@WIN-THVJGG0E067>
2024-03-03 19:50:58 +0300

    Add project files.

---
+++ Weather/DayControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for DayControl.xaml
+    /// </summary>
+    public partial class DayControl : UserControl
+    {
+        public DayControl()
+        {
+            InitializeComponent();
+        }
+        public string Day
+        {
+            get { return (string)GetValue(DayProperty); }
+            set { SetValue(DayProperty, value); }
+        }
+
+        public static readonly DependencyProperty DayProperty = DependencyProperty.Register("Day", typeof(string), typeof(DayControl));
+
+
+        public ImageSource Img
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Img", typeof(ImageSource), typeof(DayControl));
+
+        public string MinTemp
+        {
+            get { return (string)GetValue(MinNumProperty); }
+            set { SetValue(MinNumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinNumProperty = DependencyProperty.Register("MinTemp", typeof(string), typeof(DayControl));
+
+        public string MaxTemp
+        {
+            get { return (string)GetValue(MaxNumProperty); }
+            set { SetValue(MaxNumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxNumProperty = DependencyProperty.Register("MaxTemp", typeof(string), typeof(DayControl));
+
+
+    }
+}
